b0f910e2f5ae6bbe79530589e1212445bd57fad1
fregnani123 <fabianofregnani@hotmail.com>
2023-07-04 19:39:18 -0300

    estudos

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
 
 
+
+class CriaPessoa{
+    constructor(nome,sobrenome){
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+}
+
+const novaPessoa = new CriaPessoa('fabiano','Fregnani');
+
+console.log(novaPessoa)
+
