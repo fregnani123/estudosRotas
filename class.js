bcb8b9b93d5c26a2b28dc541a08998558071c580
fregnani123 <fabianofregnani@hotmail.com>
2023-07-04 22:00:36 -0300

    estudos

--- class.js
+++ class.js
@@ -2,16 +2,27 @@ class CriaPessoa{
     constructor(nome,sobrenome){
         this.nome = nome;
         this.sobrenome = sobrenome;
-    };
-
-    falaNome(){
-        console.log(`Seu nome é ${this.nome} ${this.sobrenome}, seja bem vindo!`)
+        // Object.freeze(this)
+       
     }
+    
+    falaNome(){
+        console.log(`Seu nome é ${this.nome} ${this.sobrenome},idade:${this.idade} seja bem vindo!`)
+    }
+ 
 }
 
 const args = process.argv.slice(2); 
 const nome = process.argv[2];
 const sobrenome = process.argv[3];
+const idade = process.argv[4];
+
 
 const novaPessoa = new CriaPessoa(nome, sobrenome);
+
+novaPessoa.idade = idade
+
 novaPessoa.falaNome();
+console.log(novaPessoa)
+
+
