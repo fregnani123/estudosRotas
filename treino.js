c53f54d3b54e790cbe819caf0530b1fc0ff6add6
fregnani123 <fabianofregnani@hotmail.com>
2023-07-05 19:25:12 -0300

    estudos

---
+++ treino.js
@@ -0,0 +1,18 @@
+
+
+const turma = [
+
+    {aluno:'Fabiano',
+    idade: 36},
+
+    {aluno:'Fernando',
+    idade: 33},
+
+   {aluno: 'Alline',
+    idade: 29},
+
+];
+
+for (const nomes of turma){
+    console.log(nomes.aluno)
+}
