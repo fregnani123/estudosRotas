498050a2f01ebc8ffdcdcdf05c333970fce86a41
fregnani123 <fabianofregnani@hotmail.com>
2023-07-06 16:37:36 -0300

    estudos

--- treino.js
+++ treino.js
@@ -1,3 +1,10 @@
 
 
+const ver = eSequencia('12345')
 
+function eSequencia(numeros){
+  let n = numeros
+ if (n.charAt(0).repeat(5) === n){
+    console.log('é sequencia')
+    }else{console.log('Não é sequencia')}
+}
