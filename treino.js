23a17e3e07438b846f6fcb49fa28d1d6634c73fa
fregnani123 <fabianofregnani@hotmail.com>
2023-07-07 19:25:57 -0300

    estudos

--- treino.js
+++ treino.js
@@ -1,10 +1,16 @@
 
 
-const ver = eSequencia('12345')
+// const ver = eSequencia('12345')
 
-function eSequencia(numeros){
-  let n = numeros
- if (n.charAt(0).repeat(5) === n){
-    console.log('é sequencia')
-    }else{console.log('Não é sequencia')}
-}
+// function eSequencia(numeros){
+//   let n = numeros
+//  if (n.charAt(0).repeat(5) === n){
+//     console.log('é sequencia')
+//     }else{console.log('Não é sequencia')}
+// }
+
+const formulario = document.querySelector('.formulario');
+
+formulario.addEventListener('submit',(e)=>{ if(e.type === 'submit') e.preventDefault(); console.log('Formulario não enviado.....')
+    
+})
