7c818973d1a09f1f3b486026a0aac1d2ba58a926
fregnani123 <fabianofregnani@hotmail.com>
2023-07-10 20:06:17 -0300

    estudos

--- treino.js
+++ treino.js
@@ -1,35 +1,80 @@
 
-
-// const ver = eSequencia('12345')
-
-// function eSequencia(numeros){
-//   let n = numeros
-//  if (n.charAt(0).repeat(5) === n){
-//     console.log('é sequencia')
-//     }else{console.log('Não é sequencia')}
-// }
-
-const formulario = document.querySelector('.formulario');
-const nome= document.querySelector('.nome');
-const sobrenome= document.querySelector('.sobrenome');
-const cpf= document.querySelector('.cpf')
-const senha = document.querySelector('.nome');
-const repetirSenha = document.querySelector('.repetirSenha');
-
-const nomeLabel = document.querySelector('#nome')
-
-
-formulario.addEventListener('submit',(e)=>{
- e.preventDefault(); 
-
- if (!nome.value){
-    console.log('Preencha seu nome');
- };
- 
- if(!sobrenome.value){
+class ValidaFormulario {
    
-    return
- };
+   constructor(){
+    this.formulario = document.querySelector('.formulario');
 
- formulario.submit();
-})
+  this.eventos();
+   }
+
+  eventos(){
+    this.formulario.addEventListener('submit', e => {
+      this.handleSubmit(e);
+    });
+  }
+ 
+  handleSubmit(e){
+   e.preventDefault();
+   const camposValidos = this.campoValidos();
+  }
+  
+  campoValidos() {
+   let valid = true
+   for (let campo of this.formulario.querySelectorAll('.validar')){
+      if(!campo.value) {
+         this.criaErro(campo, 'Campo tal não pode estar em branco.')
+         valid = false;
+      }
+   }
+  }
+
+  criaErro(campo,msg){
+    const div = document.createElement('div');
+    div.innerHTML = msg;
+    div.classList.add('error-text');
+    campo.insertAdjacentElement('afterend',div)
+  }
+}
+
+const valida = new ValidaFormulario();
+
+
+
+
+
+
+
+
+// // const ver = eSequencia('12345')
+
+// // function eSequencia(numeros){
+// //   let n = numeros
+// //  if (n.charAt(0).repeat(5) === n){
+// //     console.log('é sequencia')
+// //     }else{console.log('Não é sequencia')}
+// // }
+
+// const formulario = document.querySelector('.formulario');
+// const nome= document.querySelector('.nome');
+// const sobrenome= document.querySelector('.sobrenome');
+// const cpf= document.querySelector('.cpf')
+// const senha = document.querySelector('.nome');
+// const repetirSenha = document.querySelector('.repetirSenha');
+
+// const nomeLabel = document.querySelector('#nome')
+
+
+// formulario.addEventListener('submit',(e)=>{
+//  e.preventDefault(); 
+
+//  if (!nome.value){
+//     console.log('Preencha seu nome');
+//  };
+ 
+//  if(!sobrenome.value){
+   
+//     return
+//  };
+
+//  formulario.submit();
+// })
