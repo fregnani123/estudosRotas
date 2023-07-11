f363f3c86662cacc3edefd35c0faf3a2b470d6e5
fregnani123 <fabianofregnani@hotmail.com>
2023-07-11 19:20:24 -0300

    estudos

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3000
 
+app.get('/', (req,res)=> {
+    res.send('oi')
+})
+
+app.listen(PORT,() => {
+    console.log('Servidor rodando na Port:' + PORT)
+})
 
 
 
