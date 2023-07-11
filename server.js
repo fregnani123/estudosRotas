65480e8507ce66f06468d01b34276a0b9d51cd7a
fregnani123 <fabianofregnani@hotmail.com>
2023-07-11 19:38:44 -0300

    estudos

--- server.js
+++ server.js
@@ -2,10 +2,17 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const PORT = 3000
+const { execPath } = require('process');
+const PORT = 3000;
 
-app.get('/', (req,res)=> {
-    res.send('oi')
+app.use(express.static(path.join(__dirname,'public')))
+
+app.get('/ola', (req,res)=> {
+  res.sendFile(path.join(__dirname,'form.html'))
+});
+
+app.get('/css',(req,res) => {
+   res.sendFile(path.join(__dirname,'public/style.css'));
 })
 
 app.listen(PORT,() => {
