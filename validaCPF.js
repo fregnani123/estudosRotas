d10384d164304c779eeeb4a706fa1e488f728e5b
fregnani123 <fabianofregnani@hotmail.com>
2023-07-12 18:53:05 -0300

    estudos

--- validaCPF.js
+++ validaCPF.js
@@ -6,15 +6,24 @@ class ValidaCPF{
       configurable:true,
       enumerable:true,
       writable:true,
-    })
+    });
     }
+   
+    isSequential(){
+      return this.cpfLimpo.charAt(0).repeat(this.cpfLimpo.length) === this.cpfLimpo
+    }
+   
    validaCPF(){
-    if(this.cpfLimpo.length !== 11) return false
-     return ('cheguei')
+    if(!this.cpfLimpo) return false;
+    if(this.cpfLimpo.length !== 11);
+    if(this.isSequential()) return false;
+    
+  
+     return ('cheguei até aqui!')
    }
 };
 
-const cpfValido = new ValidaCPF('063.912.989-7');
+const cpfValido = new ValidaCPF('063.912.989-71');
 
 
 console.log(cpfValido.validaCPF());
